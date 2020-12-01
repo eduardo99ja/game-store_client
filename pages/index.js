a4ebf2946f3ed0b9dadf6bca8ac6d8a7c34fb585
Eduardo Apodaca <eduardo1ja99@gmail.com>
2020-11-30 22:08:35 -0600

    creating layout

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,8 @@
-
-
+import BasicLayout from '../layouts/BasicLayout'
 export default function Home() {
   return (
-    <div className="home">
-      <h1>Estamos en next</h1>
-
-    
-    </div>
+    <BasicLayout>
+      <h1>Estamos en home</h1>
+    </BasicLayout>
   )
 }
