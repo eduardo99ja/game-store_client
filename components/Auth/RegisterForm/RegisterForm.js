c977dabfd7bc4ba010ce9bd94eba172ca3920a4f
Eduardo Apodaca <eduardo1ja99@gmail.com>
2020-12-10 19:21:54 -0600

    creando formulario de registro

--- components/Auth/RegisterForm/RegisterForm.js
+++ components/Auth/RegisterForm/RegisterForm.js
@@ -1,10 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Form, Button } from 'semantic-ui-react'
 
 export default function RegisterForm({ showLoginForm }) {
   return (
-    <div>
-      <h1>Formulario de registro</h1>
-      <button onClick={showLoginForm}>Ir al login</button>
-    </div>
+    <Form className='login-form'>
+      <Form.Input name='name' type='text' placeholder='Nombre' />
+      <Form.Input name='lastname' type='text' placeholder='Apellidos' />
+      <Form.Input name='username' type='text' placeholder='Nombre de usuario' />
+      <Form.Input name='email' type='text' placeholder='Correo electronico' />
+      <Form.Input name='password' type='password' placeholder='Contraseña' />
+      <div className='actions'>
+        <Button type='button' basic>
+          Iniciar sesion
+        </Button>
+        <Button type='submit' className='submit'>
+          Registrar
+        </Button>
+      </div>
+    </Form>
   )
 }
