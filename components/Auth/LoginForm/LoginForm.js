b0b4773d3d34989de535e6ae509b9d7ec607daf2
Eduardo Apodaca <eduardo1ja99@gmail.com>
2020-12-27 21:47:56 -0600

    agregando formik y yup a formulario de login

--- components/Auth/LoginForm/LoginForm.js
+++ components/Auth/LoginForm/LoginForm.js
@@ -5,14 +5,29 @@ import * as Yup from 'yup'
 import { toast } from 'react-toastify'
 
 export default function LoginForm({ showRegisterform }) {
+  const formik = useFormik({
+    initialValues: initialValues(),
+    validationSchema: Yup.object(validationSchema()),
+    onSubmit: formData => {
+      console.log(formData)
+    },
+  })
   return (
-    <Form className='login-form'>
+    <Form className='login-form' onSubmit={formik.handleSubmit}>
       <Form.Input
         name='identifier'
         type='text'
         placeholder='Correo electronico'
+        onChange={formik.handleChange}
+        error={formik.errors.identifier}
       />
-      <Form.Input name='password' type='password' placeholder='Contraseña' />
+      <Form.Input
+        name='password'
+        type='password'
+        placeholder='Contraseña'
+        onChange={formik.handleChange}
+        error={formik.errors.password}
+      />
       <div className='actions'>
         <Button type='button' basic onClick={showRegisterform}>
           Registrarse
@@ -26,4 +41,18 @@ export default function LoginForm({ showRegisterform }) {
       </div>
     </Form>
   )
+}
+
+function initialValues() {
+  return {
+    identifier: '',
+    password: '',
+  }
+}
+
+function validationSchema() {
+  return {
+    identifier: Yup.string().email(true).required(true),
+    password: Yup.string().required(true),
+  }
 }
