ffa413e2ae01712f04b5b8728b187b2113789035
Eduardo Apodaca <eduardo1ja99@gmail.com>
2020-12-29 12:22:24 -0600

    creando pagina de usuario

--- layouts/BasicLayout/BasicLayout.js
+++ layouts/BasicLayout/BasicLayout.js
@@ -1,11 +1,18 @@
 import React from 'react'
+import classNames from 'classnames'
 import { Container } from 'semantic-ui-react'
 import Header from '../../components/Header'
 
 export default function BasicLayout(props) {
-  const { children } = props
+  const { children, className } = props
+  //className='basic-layout'
   return (
-    <Container fluid className='basic-layout'>
+    <Container
+      fluid
+      className={classNames('basic-layout', {
+        [className]: className,
+      })}
+    >
       <Header />
       <Container className='content'>{children}</Container>
     </Container>
