3a7383a7ea4535d51a509ac6da1a13185c23f71a
Eduardo Apodaca <eduardo1ja99@gmail.com>
2021-01-11 15:50:31 -0600

    manejando los datos de formulario cambio de email

--- components/Account/ChangeEmailForm/ChangeEmailForm.jsx
+++ components/Account/ChangeEmailForm/ChangeEmailForm.jsx
@@ -1,22 +1,61 @@
 import React from 'react'
 import { Form, Button } from 'semantic-ui-react'
+import { useFormik } from 'formik'
+import * as Yup from 'yup'
+import { toast } from 'react-toastify'
 
 export default function ChangeEmailForm({ user, logout, setReloadUser }) {
+  const formik = useFormik({
+    initialValues: initialValues(),
+    validationSchema: Yup.object(validationSchema()),
+    onSubmit: formData => {
+      console.log(formData)
+    },
+  })
   return (
     <div className='change-email-form'>
       <h4>
         Cambia tu e-mail <span>(Tu e-mail actual: {user.email})</span>
       </h4>
-      <Form>
+      <Form onSubmit={formik.handleSubmit}>
         <Form.Group widths='equal'>
-          <Form.Input name='email' placeholder='Tu nuevo email' />
+          <Form.Input
+            name='email'
+            placeholder='Tu nuevo email'
+            onChange={formik.handleChange}
+            value={formik.values.email}
+            error={formik.errors.email}
+          />
           <Form.Input
             name='repeatEmail'
             placeholder='Confirma tu nuevo email'
+            onChange={formik.handleChange}
+            value={formik.values.repeatEmail}
+            error={formik.errors.repeatEmail}
           />
         </Form.Group>
         <Button className='submit'>Actualizar</Button>
       </Form>
     </div>
   )
+}
+
+function initialValues() {
+  return {
+    email: '',
+    repeatEmail: '',
+  }
+}
+
+function validationSchema() {
+  return {
+    email: Yup.string()
+      .email(true)
+      .required(true)
+      .oneOf([Yup.ref('repeatEmail')], true),
+    repeatEmail: Yup.string()
+      .email(true)
+      .required(true)
+      .oneOf([Yup.ref('email')], true),
+  }
 }
